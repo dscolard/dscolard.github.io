a4983402612da7d3e63e25a3d926edec9d35b6d3
David Scolard <dscolard@tcd.ie>
2019-08-24 21:03:08 +0100

    videos added

--- map.js
+++ map.js
@@ -1,16 +1,22 @@
 
 
 var points = [
-                ["Dublin", 53.35, -6.26],
                 ["Cebu", 10.3157, 123.8854],
                 ["El Nido", 11.2097, 119.4623],
+                //moalboal oslob siquior bohol puerta princesa
+
                 ["Bangkok", 13.7563, 100.5018],
                 ["Chiang Mai", 18.7953, 98.9620],
+                // koh-tao koh-samui koh-phangan 
+
                 ["Hanoi", 21.0278, 105.8342],
                 ["Ho Chi Minh", 10.8231, 106.6297],
-                ["Budapest", 47.4979, 19.0402],
+                //phong nha halong bay danang/hoian tam coc 
+
                 ["Vancouver", 49.2827, -123.1207],
                 ["Orlando", 28.5383, -81.3792],
+                ["Budapest", 47.4979, 19.0402],
+
                 ["Madrid", 40.4168, -3.7038],
                 ["Oberstdorf", 47.4099, 10.2797],
                 ["Berlin", 52.5200, 13.4050],
@@ -23,13 +29,15 @@ var points = [
                 ["Marrakech", 31.6295, -7.9811],
                 ["Zagreb", 45.8150, 15.9819],
                 ["Mallorca", 39.6953, 3.0176],
+                ["Paris", 48.8566, 2.3522],
+
+                ["Dublin", 53.35, -6.26],
                 ["Rosslare", 52.2513, -6.3415],
                 ["Westport", 53.8021, -9.5143],
                 ["Killarney", 52.0599, -9.5044],
-                ["Paris", 48.8566, 2.3522],
+
                 ["Glasgow",55.8642,-4.2518],
                 ["Isle of Man", 54.2361, -4.5481]
-                //Lisbon 
             ];
 
 //create map object and set default positions and zoom level
